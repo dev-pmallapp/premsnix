f96ee95736a48be5f5db520d439e2ef3bea5c682
Prem Mallappa <prem.mallappa@gmail.com>
2025-10-23 13:06:22 +0000

    nix: adding gitignore --signoff

---
+++ flake.nix
@@ -0,0 +1,106 @@
+{
+  description = "Prems Nix setup";
+
+  inputs = {
+    # Core Nix ecosystem
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-master.url = "github:NixOS/nixpkgs";
+    flake-compat = {
+      url = "https://github.com/edolstra/flake-compat/archive/9100a0f413b0c601e0533d1d94ffd501ce2e7885.tar.gz";
+      flake = false;
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    # System management
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.2";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-compat.follows = "";
+      };
+    };
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    nix-rosetta-builder = {
+      # url = "github:cpick/nix-rosetta-builder";
+      url = "github:khaneliman/nix-rosetta-builder/speedfactor";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    nixos-wsl = {
+      url = "github:nix-community/nixos-wsl";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-compat.follows = "";
+      };
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
+    # Applications & packages
+    anyrun-nixos-options.url = "github:n3oney/anyrun-nixos-options";
+    catppuccin = {
+      url = "github:catppuccin/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+    hypr-socket-watch.url = "github:khaneliman/hypr-socket-watch";
+    khanelivim.url = "github:khaneliman/khanelivim";
+    nh.url = "github:nix-community/nh";
+    nixpkgs-citrix-workspace.url = "github:NixOS/nixpkgs/87894d3b7116a8e1c4827d66e17b89099d218c50";
+    nix-flatpak.url = "github:gmodena/nix-flatpak/latest";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    stylix = {
+      url = "github:danth/stylix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    waybar = {
+      url = "github:Alexays/Waybar";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-compat.follows = "";
+      };
+    };
+    yazi-flavors = {
+      url = "github:yazi-rs/flavors";
+      flake = false;
+    };
+  };
+
+  outputs =
+    inputs:
+    let
+      flake = inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+        systems = [
+          "x86_64-linux"
+          "aarch64-darwin"
+        ];
+        imports = [
+          ./flake
+        ];
+      };
+    in
+    builtins.removeAttrs flake [ "perSystem" ];
+}
